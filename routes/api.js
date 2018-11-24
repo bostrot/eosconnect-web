a35c80a97bb39cb9f0ce07d4e809376a5db8cab5
Eric <eric.trenkel@gmail.com>
2018-11-24 14:09:09 +0100

    Added global config, comments and fixed profile picture generator

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,14 @@
+// API handler
 let express = require('express');
 let router = express.Router();
 let helper = require('../helper');
 let EosEcc = require('eosjs-ecc');
 let EosApi = require('eosjs-api');
-let eos = EosApi({httpEndpoint: 'https://eos.greymass.com'});
+let config = require('../config.js')
+let eos = EosApi({httpEndpoint: config.api});
+var jimp = require('jimp');
 
+// generate profile picture with robohash.org
 router.get('/v1/avatar/:username', (req, res) => {
     let {username = "steemthebest"} = req.params;
     let {size = 128} = req.query;
@@ -16,30 +20,32 @@ router.get('/v1/avatar/:username', (req, res) => {
         image.resize(size, size).getBuffer('image/png', (err, buffer) => {
             res.set('Content-Type', 'image/png');
             res.send(buffer)
-        })
+        }) 
     })
 });
 
+// check whether app exists
 router.get('/v1/app/:client_id.exist', async (req, res) => {
     let app = await helper.db.getApp(req.params.client_id);
-    res.json({"exist": app.length === 1})
+    res.json({"exist": app !== undefined})
 });
 
+// get app info
 router.get('/v1/app/:client_id.info', async (req, res) => {
     let app = await helper.db.getApp(req.params.client_id);
-    if (app.length === 1) {
+    if (app !== undefined) {
         app = app[0]
         res.json({
             info: {
                 name: app.name,
                 description: app.description,
-                logo: 'https://eosconnect.app/api/v1/app/' + app.name + '.logo',
+                logo: config.base_url + '/api/v1/app/' + app.name + '.logo',
                 redirect_uri: JSON.parse(app.redirect_uri),
                 is_enabled: app.is_enabled,
             },
             owner: {
                 accountname: app.owner,
-                avatar: 'https://eosconnect.app/api/v1/avatar/' + app.owner
+                avatar: config.base_url + '/api/v1/avatar/' + app.owner
             }
         })
     } else {
@@ -47,15 +53,17 @@ router.get('/v1/app/:client_id.info', async (req, res) => {
     }
 });
 
+// router for app logos
 router.get('/v1/app/:client_id.logo', async (req, res) => {
     let app = await helper.db.getApp(req.params.client_id);
-    if (app.length === 1) {
+    if (app !== undefined) {
         res.redirect(app[0].logo);
     } else {
-        res.redirect('https://cdn.pixelhosting.co/jgitusuglb/f0968e0c0e2431ec2d149d6b531f817517e306a9b71f09907de7ac6a7ab406eb.jpeg');
+        res.redirect('/assets/img/eos_color.svg');
     }
 });
 
+// check public keys to verify account
 router.post('/v1/account/request', (req, res) => {
     let {accountname = null, owner_key = null, active_key = null} = req.body;
     if (!accountname || !owner_key | !active_key) {
