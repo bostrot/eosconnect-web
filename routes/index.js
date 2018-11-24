f293913d9041407640270363ee8ee05bdeeaac98
Eric <eric.trenkel@gmail.com>
2018-11-24 14:10:04 +0100

    Added postback handler and config

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,61 @@
 let express = require('express');
-let jimp = require('jimp');
 let helper = require('../helper');
 let middle = require('./middle');
 let EosEcc = require('eosjs-ecc');
 let EosApi = require('eosjs-api');
-let eos = EosApi({httpEndpoint: 'https://eos.greymass.com'});
+let config = require('../config');
+let eos = EosApi({httpEndpoint: config.api});
 let jwt = require('jsonwebtoken');
 
 let router = express.Router();
 
+// load basic dashboard layout with login button
 router.get('/', async (req, res) => {
-
     res.render('dashboard', {
         data: null,
-        login_url: process.env.env === 'dev' ? '/auth?client_id=example.app&scopes=login&redirect_uri=http://localhost:3000/int/login' : '/auth?client_id=eosconnect&scopes=login&redirect_uri=https://eosconnect.app/int/login'
+        login_url: process.env.env === 'dev' ? '/auth?client_id=example.app&scopes=login&redirect_uri='+config.base_url+'/int/login' : 
+            '/auth?client_id=eosconnect&scopes=login&redirect_uri='+config.base_url+'/int/apps'
     })
-
 });
 
+// coinpayments.net trade postback url - create sqlite db entry for new app
+router.post(config.postback, async (req, res) => {
+    try {
+        var returned = req.body.custom.split(',');
+        if (parseInt(req.body.status) >= 100 && parseInt(req.body.amount1) >= 5) {
+            console.log('Payment successfully received.')
+            helper.db.prepare('INSERT INTO apps (name, description, logo, redirect_uri, owner) VALUES (?,?,?,?,?)')
+                .run([
+                    returned[0], // name
+                    returned[1], // description
+                    returned[2], // logo
+                    returned[3], // redirect urls
+                    returned[4], // username
+                ]);
+        }
+    } catch (e) {
+        console.log(req.body)
+    }
+    res.send("OK")
+});
+
+// basic api auth scope
 router.get('/auth', middle.validateAuth, (req, res) => {
     let scope_description = [];
     res.scopes.forEach(scope => {
         switch (scope) {
             case 'login':
-                scope_description.push("Verify your EOS identity");
+                scope_description.push("verify your EOS identity");
                 break;
             case 'accountcreate':
-                scope_description.push("Create new EOS accounts"); //not implemented
+                scope_description.push("create new EOS accounts"); //not implemented
                 break;
         }
     });
     res.render('auth', {scope_description, client: res.client, scopes: res.scopes, redirect_uri: res.redirect_uri});
 });
 
+// revoke current active token
 router.post('/auth/token/revoke', async (req, res) => {
     let {authorization = null} = req.headers;
     let token = await helper.db.getToken(authorization);
@@ -45,6 +68,7 @@ router.post('/auth/token/revoke', async (req, res) => {
     }
 });
 
+// authenticate with post request and validate keys
 router.post('/auth', (req, res) => {
     let {client = null, redirect_uri = null, scopes = null} = req.session;
     let {username = null, pass = null} = req.body;
@@ -64,7 +88,7 @@ router.post('/auth', (req, res) => {
             }
         }).catch(error => {
             res.render('error/unknown_account');
-            console.log("error invalid account")
+            console.log("error invalid account: " + error)
         });
     }
 
